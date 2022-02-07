a4c5b8f28035e71a5e4c2e17075a0e8f5772744f
Dofah <paul.andanson@gmail.com>
2022-02-07 21:13:19 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
+    <link href="css/style.css" type="text/css" rel="stylesheet">
+    
+
+</head>
+
+
+<header>
+    
+
+    <div class="gauche">
+        <h1 class="titre">craft.</h1>
+    </div>
+  
+    <div class="droite">
+        <a href="google.fr" class="lien">Why</a>
+        <a href="google.fr" class="lien">Get Started</a>
+        <a href="google.fr" class="lien">Docs</a>    
+    </div>
+    
+</header>
+
+
+
+<section class="section">
+    <div class="gauche2">
+        <h1 class="titresection">The easy to use tool made for developers, by developers.</h1>
+        <br>
+        <p class="soustitre">With Craft, you can easily create new mods for multiple platforms with high-level programming.</p>
+    </div>
+
+
+   
+    <img src="css/main.svg" class="img">
+  
+
+
+
+</section>
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+</html>
